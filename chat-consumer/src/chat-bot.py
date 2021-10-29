5f6326a2ca7b39715390314351bb2a805555d36a
KaioViana <kaiu.viana@hotmail.com>
2021-10-29 13:38:33 -0300

    bot twitch conectado

---
+++ chat-consumer/src/chat-bot.py
@@ -0,0 +1,37 @@
+from twitchio.ext import commands
+from dynaconf import settings
+import json
+
+
+class Bot(commands.Bot):
+
+    def __init__(self):
+        super().__init__(token=settings.TWITCH_TOKEN, client_id=settings.TWITCH_CLIENT_ID, nick='ROBOT_DEV', prefix='!',
+                         initial_channels=['Gaules'])
+
+    # Events don't need decorators when subclassed
+    async def event_ready(self):
+        print(f'Ready | {self.nick}')
+
+    async def event_message(self, message):
+        payload = {
+          'id': str(message.id),
+          'author': str(message.author.name),
+          'content': str(message.content),
+          'tags': str(message.tags),
+        }
+
+
+        payload_json = json.dumps(payload)
+        print(payload_json)
+        await self.handle_commands(message)
+
+    # Commands use a decorator...
+    @commands.command(name='test')
+    async def my_command(self, ctx):
+        await ctx.send(f'Hello {ctx.author.name}!')
+
+
+if __name__ == '__main__':
+    bot = Bot()
+    bot.run()
