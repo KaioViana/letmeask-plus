3876fa1877099049eff864c01978d34186c73a7c
KaioViana <kaiu.viana@hotmail.com>
2021-11-01 11:41:07 -0300

    removendo console logs

--- chat-consumer/src/firebase/config.py
+++ chat-consumer/src/firebase/config.py
@@ -3,12 +3,10 @@ from dynaconf import settings
 from firebase_admin import credentials
 from firebase_admin import db
 
-print('imported')
+
 def connect():
   cred = credentials.Certificate(settings.FIREBASE_CONFIG_PATH)
 
   firebase_admin.initialize_app(cred, { 'databaseURL': settings.FIREBASE_DATABASE_URL })
-
-  print('dataBaseInitialized******')
-
+  
   return db
